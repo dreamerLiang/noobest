e06443ca73025ee7882673b44278b0128036f84a
MrHalf <luyuhao627@gmail.com>
2016-04-23 11:10:31 -0400

    fix minor indentation error stuff

--- noobest/websites/views.py
+++ noobest/websites/views.py
@@ -3,26 +3,25 @@ from django.shortcuts import render, HttpResponseRedirect
 
 
 def index(request):
-	return render(request, "index.html", locals())
+    return render(request, "index.html", locals())
 def about(request):
-	#return HttpResponseRedirect("about.html")
-	return render(request, "about.html", locals())
+    return render(request, "about.html", locals())
 
 def summoner(request):
-	print "returning result"
-	if request.is_ajax():
-		#deal with the user input here.
-		# rank(user)
-		# redirect to result display page
-		return render(request, "result.html", locals())
-	return render(request, "search.html", locals())
+    print "returning result"
+    if request.is_ajax():
+        #deal with the user input here.
+        # rank(user)
+        # redirect to result display page
+        return render(request, "result.html", locals())
+    return render(request, "search.html", locals())
 
 def search(request):
     if request.is_ajax():
         #deal with the user input here.
         if not 'name' in request.POST:
         #return false
-
+            print 'hello'
         #get user name
         name = request.POST.get('name')
         # check if the name exist
