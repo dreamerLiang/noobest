79546387b2fa40c1d0b295381dd340a945f2f32f
Liang Zhang <506425878@qq.com>
2016-04-23 23:16:51 -0400

    add code to main logic part

--- noobest/websites/views.py
+++ noobest/websites/views.py
@@ -1,6 +1,7 @@
 from utils.api import get_watcher
 from rank.views import rank
 from django.shortcuts import render, HttpResponseRedirect
+from utils.constants import ErrorList
 
 import operator
 
@@ -31,7 +32,7 @@ def search(request):
             error_code = ErrorList.USER_NOT_FOUND
             return render(request, "search.html", locals())
         match = get_watcher().get_match_list(me['id'],'na')
-        match_id_list = [i['matchId'] for i in match['matches'] if i['queue'] == 'TEAM_BUILDER_DRAFT_RANKED_5x5'][:15]
+        match_id_list = [i['matchId'] for i in match['matches'] if i['queue'] == 'TEAM_BUILDER_DRAFT_RANKED_5x5'][:20]
         match_details = []
         all_players_id = dict()
 
@@ -39,14 +40,36 @@ def search(request):
             match_detail = get_watcher().get_match(match_id)
             match_details.append(match_detail)
             for data in match_detail['participantIdentities']:
-                if data['player']['summonerId'] in all_players_id:
-                    all_players_id[data['player']['summonerId']] += 1
+                if data['player']['summonerName'] in all_players_id:
+                    all_players_id[data['player']['summonerName']].append([match_detail, data['participantId']])
                 else:
-                    all_players_id[data['player']['summonerId']] = 1
+                    all_players_id[data['player']['summonerName']] = [[match_detail, data['participantId']]]
 
-        sorted_all_players_id = sorted(all_players_id.items(), key=operator.itemgetter(1))
-        friends_id = [key for key, value in sorted_all_players_id if value > 1]
+        friends_id = [key for key, value in all_players_id.items() if len(value) > 1]
+        print "Friend id:\n"
         print friends_id
+
+        for fid in friends_id:
+            #print all_players_id[fid]
+            for data in all_players_id[fid]:
+                #print data
+                specific_match = data[0]
+                #print specific_match
+                for participants in specific_match['participants'][0]['stats']:
+                    print specific_match['participants'][0]['stats'][participants]    
+                
+        # for match_detail in match_details:
+        #     for data in match_detail['participantIdentities']:
+        #         if data['player']['summonerName'] in friends_id:
+        #             print data['player']['summonerName']
+
+        #for friends_id_object in friends_id:
+        #     for data in match_detail['participantIdentities']:
+        #         if data['player']['summonerId'] == friends_id_object:
+        #             print friends_id_object
+                #summonerId_list_in_Game = match_detail['summonerID']
+                # if found:      
+                #     print 'found'
         return render(request, "result.html", locals())
  
         # if not exists('name'):
